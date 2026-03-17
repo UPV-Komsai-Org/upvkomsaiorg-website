878265d201e0f95f43bfb6dd010e87e0e180bbab
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-03-17 19:02:36 +0800

    refactor: serialize event data for easy modification

--- events/scripts/calendar.js
+++ events/scripts/calendar.js
@@ -7,24 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
       buttonText: {
         today: 'Today' 
       },
-      events: [
-        {
-          title: 'Event 1',
-          start: '2026-02-10',
-          end: '2026-02-12'
-        },
-        {
-          title: 'Event 2',
-          start: '2026-02-15',
-          end: '2026-02-16'
-        },
-        {
-          title: 'Event 3',
-          start: '2026-02-20',
-          end: '2026-02-22'
-        }
-
-      ]
+      events: "./data/calendar-tags.json"
     });
     calendar.render();
 });
