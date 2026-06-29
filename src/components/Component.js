6c4efe90e01ba2373fdd69c8a0ff17c3737cb56c
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-06-29 20:56:15 +0800

    feat: added home page layout

---
+++ src/components/Component.js
@@ -0,0 +1,16 @@
+/**
+ * This contains all the components for importing instead of importing them one by one.
+ */
+
+import Button from './Button.astro';
+import Card from './Card.astro';
+import Footer from './Footer.astro';
+import HeroSection from './HeroSection.astro';
+import Line from './Line.astro';
+import Link from './Link.astro';
+import Media from './Media.astro';
+import Navbar from './Navbar.astro';
+import Section from './Section.astro';
+
+export { Button, Card, Footer, HeroSection, Line, Link, Media, Navbar, Section };
+
