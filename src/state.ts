8fc495e4efc382071cefe0bd022bbe70e56a8fcd
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-07-01 11:51:18 +0800

    docs: added and simplified documentation for component files, global css, and states

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,11 @@
-// i dont know what happened
+/**
+ * @file state.ts
+ * @description This file contains the state of the website. It is used to store the current page of the website.
+ * @note By default, it points to the index.astro page.
+ * @note Relative paths must have "import.meta.env.BASE_URL" at the start of the path/URL. 
+ *      This is due to hosting sometimes being confused with URLs and paths. Refer to examples from the pages files.
+ */
+
 import { atom } from "nanostores";
 
 export const href = atom(`${import.meta.env.BASE_URL}`);
